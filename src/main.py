608adbf2e9fbee65732c0602278b097f8b3cb303
maker2413 <maker2413@gmail.com>
2025-01-15 08:51:44 -0600

    Almost got static site generator working

--- src/main.py
+++ src/main.py
@@ -1,13 +1,17 @@
-from htmlnode import HTMLNode
-from textnode import *
+import os
+import shutil
+
+from copy_static import copy_files_recursive
+
+dir_path_static = "./static"
+dir_path_public = "./public"
 
 def main():
-    text_node = TextNode("This is a text node", "bold", "https://www.boot.dev")
-    html_node = HTMLNode("a", "Google", props={"href": "https://www.google.com", "target": "_blank"})
-    print(text_node)
-    print(html_node)
+    print("Deleting public directory...")
+    if os.path.exists(dir_path_public):
+        shutil.rmtree(dir_path_public)
 
-    print(html_node.props)
-    print(html_node.props_to_html())
+    print("Copying static files to public directory...")
+    copy_files_recursive(dir_path_static, dir_path_public)
 
 main()
