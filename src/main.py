b06bec819dc8d2906304b977e8b118dee40bde5b
maker2413 <maker2413@gmail.com>
2025-01-15 09:15:13 -0600

    Completed static site generator!

--- src/main.py
+++ src/main.py
@@ -2,9 +2,12 @@ import os
 import shutil
 
 from copy_static import copy_files_recursive
+from gen_content import generate_pages_recursive
 
 dir_path_static = "./static"
 dir_path_public = "./public"
+dir_path_content = "./content"
+template_path = "./template.html"
 
 def main():
     print("Deleting public directory...")
@@ -13,5 +16,8 @@ def main():
 
     print("Copying static files to public directory...")
     copy_files_recursive(dir_path_static, dir_path_public)
+
+    print("Generating content...")
+    generate_pages_recursive(dir_path_content, template_path, dir_path_public)
 
 main()
